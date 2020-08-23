963e48eb6f00143dbedd81c78bf733d65817c3dd
James Gallagher <jamesg@jamesg.app>
2020-08-23 15:38:53 +0100

    feat: Update project

--- app.py
+++ app.py
@@ -4,34 +4,50 @@ import datetime
 import os
 import jinja2
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 def get_data():
-	token = "keyFiaoGoLBiymZB7"
+	token = os.environ.get("token")
 	
 	auth = { "Authorization": "Bearer {}".format(token) }
 
-	attributes = requests.get('https://api.airtable.com/v0/appoPJiqsrQ3BSRto/Coffees?view=All%20Brews', headers=auth)
+	attributes = requests.get("https://api.airtable.com/v0/appoPJiqsrQ3BSRto/Coffees?view=All%20Brews", headers=auth)
 
 	attributes_as_json = attributes.json()
 
 	return attributes_as_json
 
 def create_page(data):
-	with open('template.html') as file_:
+	with open("template.html") as file_:
 	    template = jinja2.Template(file_.read())
 
-	today = datetime.datetime.today()
+	yesterday = datetime.datetime.today() - datetime.timedelta(days=1)
 
-	today_formatted = today.strftime("%A %B %d, %Y")
+	day_before_yesterday = datetime.datetime.today() - datetime.timedelta(days=2)
 
-	today_coffees = []
+	yesterday_formatted = yesterday.strftime("%A %B %d, %Y")
 
-	coffees_today = len(today_coffees)
+	yesterday_coffees = [d for d in data["records"] if d["fields"]["Date"] == yesterday.strftime("%Y-%m-%d")]
 
-	for d in data["records"]:
-		if d["fields"]["Date"] == today.strftime("%Y-%d-%m"):
-			today_coffees.append(d)
+	coffees_yesterday = len(yesterday_coffees)
 
-	final = template.render(coffees=data["records"], today=today_formatted, coffees_today=coffees_today)
+	at_home = [c for c in yesterday_coffees if c["fields"]["Coffee Shop"] == "Home"]
+
+	at_home_count = len(at_home)
+
+	coffees_day_before_yesterday = [d for d in data["records"] if d["fields"]["Date"] == day_before_yesterday.strftime("%Y-%m-%d")]
+
+	coffees_day_before_yesterday_count = len(coffees_day_before_yesterday)
+
+	final = template.render(
+		coffees=data["records"],
+		yesterday=yesterday_formatted,
+		coffees_yesterday=coffees_yesterday,
+		at_home_count=at_home_count,
+		coffees_day_before_yesterday_count=coffees_day_before_yesterday_count
+	)
 
 	with open("index.html", "w+") as index_page:
 		index_page.write(final)
